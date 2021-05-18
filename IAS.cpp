a39892e7d0e655c4d34b4494feb8c2c61e7bf1e5
poorneshwar9441 <79003983+poorneshwar9441@users.noreply.github.com>
2021-05-18 15:46:38 +0530

    file upgraded

--- IAS.cpp
+++ IAS.cpp
@@ -14,7 +14,7 @@ ll Ram2[1000];   // Ram locations 500-999 reserverd for data
 
 /**
  
-[[op,data, op, data];
+[[op,data, op, data]] structure of instruction [op data]
  
 
 **/
@@ -37,8 +37,10 @@ public:
   ll AC; 
   ll MQ;
   ll MBR;
-
-
+  int current_instuction; // 0 for left and 1 for Right
+  int skip_right;
+  int skip_left;
+  ll PREV_IR;
 
   void initiliaze()
   {
@@ -50,6 +52,10 @@ public:
      this->MQ = 0;
      this->MBR = 0;
      this->MAR = 0;
+     this->current_instuction = 0;
+     this->skip_right = 0;
+     this->skip_right = 0;
+     this->PREV_IR = 0;
 
 
   }
@@ -60,11 +66,22 @@ public:
      this->MBR = ::Ram[this->PC][1];//left d ata  
      this->IBR[0] = ::Ram[this->PC][2];// Rightopcode
      this->IBR[1] = ::Ram[this->PC][3];//Right data;
-     execute(); // left instuction;
-     IR = IBR[0];
-     MBR = IBR[1];
-     execute();//Right instruction;
+
+     ll current_PC = this->PC;
+
+    if((this->PREV_IR != 14) && (this->PREV_IR != 16))
+    { 
+      execute(); 
+    }
+    if(this->PC == current_PC)
+    { 
+          
+     this->IR = IBR[0];
+     this->MBR = IBR[1];
+     execute();     //Right instruction;
      this->PC += 1;
+    }
+    this->PREV_IR = this->IR;
      
   }
   void execute()
@@ -74,66 +91,91 @@ public:
      {
 
     // Data Transfer Instructions
-         case(10)://LOAD MQ
+         case(10):
              this->AC = this->MQ;
              break;
-         case(9)://LOAD MQ,M(X)
+         case(9):
               this->MQ = Ram2[this->MBR];
               break;
 
-         case(33)://STOR M(X)
+         case(33):
               Ram2[this->MBR] = this->AC;
               break;
 
-         case(1)://LOAD M(X)
+         case(1):
               this->AC = Ram2[this->MBR];
               break;
 
-         case(2)://LOAD -M(X)
+         case(2):
               this->AC = Ram2[this->MBR]*-1;
               break;
 
-         case(3)://LOAD |M(X)|
+         case(3):
              this->AC = abs(Ram2[this->MBR]);
              break;
 
-         case(4)://LOAD -|M(X)|
+         case(4):
              this->AC = -1*abs(Ram2[this->MBR]);
              break;
 
       // Arthematic Instructions
-         case(5)://ADD M(X)
+         case(5):
             this->AC = this->AC+Ram2[this->MBR];
             break;
 
-         case(7)://ADD M(X)
+         case(7):
             this->AC = this->AC+abs(Ram2[this->MBR]);
             break;
 
-         case(6)://SUB M(X)
+         case(6):
             this->AC = this->AC-(Ram2[this->MBR]);
             break;
 
-         case(8)://SUB |M(X)|
+         case(8):
             this->AC = this->AC - abs(Ram2[this->MBR]);
             break;
 
-         case(11)://MUL M(X)
+         case(11):
             this->AC = this->MQ * Ram2[this->MBR];
             break;
 
-         case(12)://DIV M(X)
+         case(12):
             this->AC = (this->AC)%(Ram2[this->MBR]);
             this->MQ = this->AC / Ram2[this->MBR];
             break;
 
-         case(20)://LSH
+         case(20):
             this->AC = this->AC *2;
             break;
 
-         case(21)://RSH 
+         case(21):
             (this->AC) = (this->AC)/2;
             break;
+
+        // Unconditional_Branch
+         case(13): 
+            (this->PC) = this->MBR;
+             break;
+
+         case(14):
+             (this->PC) = this->MBR;
+              break;
+
+        // conditional_Branch
+          case(15):
+              if(this->AC > 0)
+              {
+                 this->PC = this->MBR;
+              }
+              break;
+
+          case(16):
+              if(this->AC > 0)
+              {
+                this->PC = this->MBR;
+              }
+              break;
+
 
      }
 
@@ -144,27 +186,54 @@ public:
 int main(int argv,char const *s[])
 { 
   
-// program 
+// program to calculate factorial of a given number
   Ram[0][0] = 9;
-  Ram[0][1] = 501;
-  Ram[0][2] = 10;
-  Ram[0][3] = 0;
+  Ram[0][1] = 502;
+  Ram[0][2] = 11;
+  Ram[0][3] = 600;
 
   Ram[1][0] = 33;
-  Ram[1][1] = 501;
-  Ram[1][2] = 5;
-  Ram[1][3] = 501;
+  Ram[1][1] = 600;
+  Ram[1][2] = 1;
+  Ram[1][3] = 502;
 
-  Ram2[501] = -20;
+  Ram[2][0] = 5;
+  Ram[2][1] = 503;
+  Ram[2][2] = 33;
+  Ram[2][3] = 502;
+
+  Ram[3][0] = 1;
+  Ram[3][1] = 501;
+  Ram[3][2] = 6;
+  Ram[3][3] = 503;
+
+  Ram[4][0] = 33;
+  Ram[4][1] = 501;
+  Ram[4][2] = 1;
+  Ram[4][3] = 501;
+
+  Ram[5][0] = 15;
+  Ram[5][1] = 0;
+
+  Ram2[600] = 1;
+  Ram2[501] = 10; // number to find factorial
+  Ram2[502] = 1;
+  Ram2[503] = 1;
 // end
 
   Cpu c;
+  c.initiliaze();
+  //cout << c.PC << endl;
   while(c.PC < 500)
   {
+    cout << "PC:" <<c.PC  << " ";
+    cout << "AC:" <<c.AC  << " ";
+    cout << "MQ:" <<c.MQ << " ";
+    cout << "Ram2: "<< Ram2[600] << "  " << endl;
     c.decode();
-  }
 
-  cout << c.AC << endl;
-  cout << c.MQ << endl;
+  //cout << "hello" << endl;
 
+}
+cout << Ram2[600] << endl;
 }
